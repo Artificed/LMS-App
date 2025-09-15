050de3dc196c2a4b436e30706dcbcaaa7ebe131b
Artificed <paultsai.id@gmail.com>
2025-09-16 05:35:20 +0700

    build: Backend entrypoint

--- Backend/entrypoint/entrypoint.sh
+++ Backend/entrypoint/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Running Prisma migrations..."
-npx prisma migrate deploy
-
 echo "Generating Prisma client..."
 npx prisma generate
+
+echo "Running Prisma migrations..."
+npx prisma migrate deploy
 
 echo "Running database seed script..."
 npx prisma db seed
